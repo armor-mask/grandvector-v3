b17ce61e814ccfa4f577d71990ae7b929854b547
armor-mask <milojevica999@gmail.com>
2025-09-02 15:16:52 +0200

    Restore Complete Website with All Sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import Hero from './components/Hero';
+import TrustedBy from './components/TrustedBy';
+import Services from './components/Services';
+import About from './components/About';
+import LeadGeneration from './components/LeadGeneration';
+import Development from './components/Development';
+import Portfolio from './components/Portfolio';
+import Testimonials from './components/Testimonials';
+import FAQ from './components/FAQ';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="min-h-screen bg-black text-white overflow-x-hidden">
       <Hero />
+      <TrustedBy />
+      <Services />
+      <About />
+      <LeadGeneration />
+      <Development />
+      <Portfolio />
+      <Testimonials />
+      <FAQ />
+      <Contact />
+      <Footer />
     </div>
   );
 }
