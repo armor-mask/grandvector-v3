e6c5f28af5f3861d7144be5aca0fb98bc47eb0d1
armor-mask <milojevica999@gmail.com>
2025-09-02 20:49:19 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Hero from './components/Hero';
 import TrustedBy from './components/TrustedBy';
-import Services from './components/Services';
 import About from './components/About';
+import Services from './components/Services';
 import LeadGeneration from './components/LeadGeneration';
 import Development from './components/Development';
-import Portfolio from './components/Portfolio';
 import Testimonials from './components/Testimonials';
+import Portfolio from './components/Portfolio';
 import FAQ from './components/FAQ';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -16,12 +16,12 @@ function App() {
     <div className="min-h-screen bg-black text-white overflow-x-hidden">
       <Hero />
       <TrustedBy />
-      <Services />
       <About />
+      <Services />
       <LeadGeneration />
       <Development />
-      <Portfolio />
       <Testimonials />
+      <Portfolio />
       <FAQ />
       <Contact />
       <Footer />
